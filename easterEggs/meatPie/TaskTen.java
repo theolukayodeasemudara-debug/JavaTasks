1eeb65ea3feb1f1b542a8ee2bee5ad62209baac7
Olukayode <theolukayodeasemudara@gmail.com>
2026-04-04 00:50:54 +0100

    commit all the meatpieSnacks

---
+++ easterEggs/meatPie/TaskTen.java
@@ -0,0 +1,9 @@
+import java.util.Scanner; 
+public class TaskTen{
+                public stativ void main(String[] args) {
+                    Scanner valueCollector = new Scanner(System.in);
+
+                    System.out.println("What's your first number?");
+                    int firstValue = valueCollector.nextInt();   
+                }
+}
