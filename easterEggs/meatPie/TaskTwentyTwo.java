21c508ca6c133d9a3bd772344bea20962e28227f
Olukayode <theolukayodeasemudara@gmail.com>
2026-04-04 09:45:12 +0100

    another commit

--- easterEggs/meatPie/TaskTwentyTwo.java
+++ easterEggs/meatPie/TaskTwentyTwo.java
@@ -5,10 +5,10 @@ public class TaskTwentyTwo{
                                         Scanner valueCollector = new Scanner(System.in);    
                                         System.out.println("Give me a radius value!");
 
-                                        double rad = valueCollector.nextInt();
+                                        double rad = valueCollector.nextDouble();
                                         float valOfPi = 3.142f;
 
-                                        System.out.printf("The Area a Circle with radius:%d, with val of Pi:%d is:",rad,valOfPi);
-                                        System.out.println(3.142*rad*rad);   
+                                        System.out.printf("The Area of a Circle with radius:%f, with val of Pi:%f is:",rad,valOfPi);
+                                        System.out.println(3.142*rad*rad);
                        }
 }
