c06e2c50dea3c2d087b05ac55d469321bcbfec3b
Olukayode <theolukayodeasemudara@gmail.com>
2026-04-05 15:56:32 +0100

    another one

--- easterEggs/hotDogs/GratuityCalculator.java
+++
@@ -1,8 +0,0 @@
-public class GratuityCalculator{
-                public static void main (String[] args) {
-    
-                }
-}
-
-
-
--- easterEggs/hotDogs/VolOfPrism.java
+++ easterEggs/hotDogs/VolOfPrism.java
@@ -3,13 +3,19 @@ public class VolOfPrism {
                 public static void main (String[] args) {
                         Scanner inputCollector = new Scanner(System.in);
 
-                        System.out.println("What mile do you want to convert to Kilometer?");
-                        int value = inputCollector.nextInt(); 
+                        System.out.println("What is the length of the prism?");
+                        int length = inputCollector.nextInt(); 
 
-                        int calcOne = value/2;
-                        int calcTwo = calcOne/4;
-                        int toKilometer = calcOne + calcTwo;
+                        System.out.println("What is the base of the prism?");
+                        int base = inputCollector.nextInt(); 
 
-                        System.out.printf("%d to kilometer is %d %n%n",value,toKilometer);  
+                        float calcBaseArea = 0.5f * length * base;
+                        float calVol = calcBaseArea * length;
+
+                        System.out.printf("The volume of your prism therefore is %f %n%n",calVol);  
                 }
 }
+
+
+//                  base area formula = 1/2bh
+//                  vol of prism = area * length
