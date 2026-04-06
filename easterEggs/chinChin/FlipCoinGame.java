df8341e92c850281797343bf0bec06959f263a0f
Olukayode <theolukayodeasemudara@gmail.com>
2026-04-06 12:26:21 +0100

    another commit

--- easterEggs/chinChin/FlipCoinGame.java
+++ easterEggs/chinChin/FlipCoinGame.java
@@ -3,15 +3,22 @@ public class FlipCoinGame {
             public static void main (String[] args) {
                         Scanner userInput = new Scanner(System.in);
 
+//                      User instructions
                         System.out.println("Pick a number between 0 and 1 to play this game of flipping a coin");
                         int playerNum = userInput.nextInt();
 
+//                      Generating a number between 0 and 1
+                         int randomNum = (int)(Math.random() * 1);
+
+//                      coin sides
+                         String up = "Head";
+                         String down = "Tail";
+
 //                      checking player input
-                        if(playerNum == 0) {
-                            System.out.printf("You chose %d which is: 'Head' %n%n'", playerNum);
-                        } 
-                        else if(playerNum == 1) {
-                            System.out.printf("You chose %d, which is: 'Tail' %n%n'", playerNum);
+                        if(randomNum == 1) {
+                            System.out.printf("No chosen = %d : Comp chose = %d: %s %n%n' You Won!!! %n",playerNum,randomNum,up);
+                        } else if(randomNum == 0) {
+                            System.out.printf("No chosen = %d : Comp chose = %d: %s %n%n' You lost!!! %n",playerNum,randomNum,down);
                         }
             }
 }
--- easterEggs/chinChin/RainbowColorDisplay.java
+++ easterEggs/chinChin/RainbowColorDisplay.java
@@ -1,33 +1,25 @@
 public class RainbowColorDisplay {
                 public static void main (String[] args) {
 //                    generating random numbers
-                      int randomNum = (int)(Math.random() * 7);
+                      int randomNum = (int)(Math.random() * 7) + 1;
 
 //                    displaying the colors based on the numbers displayed
-                      if(randomNum == 0) {               
-                      System.out.printf("%d means no color is picked %n%n", randomNum);
-                       }
-                      if(randomNum == 1) {               
-                      System.out.printf("%d is violet %n%n", randomNum);
-                       }
-                      else if(randomNum == 2) {               
-                      System.out.printf("%d is indigo %n%n", randomNum);
-                       }
-                      else if(randomNum == 3) {               
-                      System.out.printf("%d is blue %n%n", randomNum);
-                       }
-                      else if(randomNum == 4) {               
-                      System.out.printf("%d is green %n%n", randomNum);
-                       }
-                      else if(randomNum == 5) {               
-                      System.out.printf("%d is yellow %n%n", randomNum);
-                       }
-                      else if(randomNum == 6) {               
-                      System.out.printf("%d is orange %n%n", randomNum);
-                       }
-                      else if(randomNum == 7) {               
-                      System.out.printf("%d is red %n%n", randomNum);
-                       }
+   
+                      if(randomNum == 1){
+                            System.out.printf("The color picked is 'Violet': %d %n", randomNum);
+                      } else if(randomNum == 2){
+                            System.out.printf("The color picked is 'Indigo': %d %n", randomNum);
+                     } else if(randomNum == 3){
+                            System.out.printf("The color picked is 'Blue': %d %n",randomNum);                     
+                     } else if(randomNum == 4){
+                            System.out.printf("The color picked is 'Green': %d%n", randomNum);
+                    } else if(randomNum == 5){
+                            System.out.printf("The color picked is 'Yellow': %d%n", randomNum);
+                    } else if(randomNum == 6){
+                            System.out.printf("The color picked is 'Orange': %d%n", randomNum);        
+                    } else if(randomNum == 7){
+                            System.out.printf("The color picked is 'Red': %d", randomNum);
+                    }
                 }
 }
 
