99eea82a3b2070d27c62da03c4517968207e42c7
Olukayode <theolukayodeasemudara@gmail.com>
2026-04-09 07:26:36 +0100

    fixed a never ending loop

--- littleLoops/PrintFromNToOne.java
+++ littleLoops/PrintFromNToOne.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-public class PrintUserNeg {
+public class PrintFromNToOne {
         public static void main (String[] args) {
                 Scanner inputCollector = new Scanner(System.in);
 
 //              Enter your number
                 System.out.println("Enter your number!");
-                int userInput = inputCollector.nextInt();
+                int userValue = inputCollector.nextInt();
 
-                for (int i = 1; i <=userInput; i--) System.out.println(i+userInput);
+                for (int i = userValue; i >=1; i--) System.out.println(i);
         }
 }
