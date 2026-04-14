287afe81bab879daa00b81ffbe451eb9f6e15cf2
Olukayode <theolukayodeasemudara@gmail.com>
2026-04-14 18:16:14 +0100

    replaced \n syntax with block quotes for easy readability

--- nokiaPhone/NokiaPhone.java
+++ nokiaPhone/NokiaPhone.java
@@ -4,69 +4,229 @@ public class NokiaPhone {
         Scanner scanner = new Scanner(System.in);
         
         // menu options from one 1 - 13
-        String mainMenu = "select main menu\n\n1.Phone Book\n2.Messages\n3.Chat\n4.Call register\n5.Tones\n6.Settings\n7.Call divert\n8.Games\n9.Calculator\n10.Reminders\n11.Clock\n12.Profiles\n13.SIM services\n";
-        System.out.println(mainMenu+"\n");
+        String mainMenu = 
+        """
+        Main menu
+        1.Phone book
+        2.Messages
+        3.Chat
+        4.Call register
+        5.Tones
+        6.Settings
+        7.Call divert
+        8.Games
+        9.Calculator
+        10.Reminders
+        11.Clock
+        12.Profiles
+        13.SIM services
+        
+        select your option
+        """;
+        System.out.print(mainMenu);
         
         // phoneBook menu options
-        String phoneBook = "Phone book\n1.Search\n2.Service Nos\n3.Add name\n4.Erase\n5.Edit\n6.Assign tone\n7.Send b'card\n8.Options\n9.Speed dials\n10.Voice tags\n";
-        String phoneSubMenu = "\n1.Type of view\n2.Memory status\n0. to return";
+        String phoneMenu =
+        """
+        Phone Book
+        1.Search
+        2.Service Nos
+        3.Add name
+        4.Erase
+        5.Edit
+        6.Assign tone
+        7.Send b'card
+        8.Options
+        9.Speed dials
+        10.Voice tags
+        0.Go back
+        
+        select option
+        """;
+        
+        String phoneSubMenu =
+        """
+        Options
+        1.Type of view
+        2.Memory status
+        0.Go back
+        
+        select option
+        """;
         
         // phone options subMenumenu
-        String options = "Options\n1.Type of view\n2.Memory status\n";
+        String options =
+        """
+        Options
+        1.Type of view
+        2.Memory status
+        0.Go back
+        
+        select option
+        """;
         
         // messages menu
-        String messages = "Messages\n1.Write messages\n2.Inbox\n3.Outbox\n4.Picture messages\n5.Templates\n6.Smileys\n7.Message settings\n8.Info service\n9.Voice mailbox\n10.Service command editor\n";
+        String messages =
+        """
+        Messages
+        1.Write message
+        2.Inbox
+        3.Outbox
+        4.Picture messages
+        5.Templates
+        6.Smileys
+        7.Message settings
+        8.Info service
+        9.Voice mailbox
+        10.Service command editor
+        
+        select an option
+        """;
+        
+        String mSubMenu = 
+        """
+        1. Write messages 
+        2. Inbox 
+        3. Outbox 
+        4. Picture messages 
+        5. Templates 
+        6. Smileys 
+        7. Message settings 
+        
+        select option
+        """;
         
         // chat menu
-        String chat = "Chat\n1.Chat\n";
+        String chat = 
+        """
+        Chat
+        1.Chat
+        
+        select option
+        """;
         
         // call register menu
-        String callRegister = "Call register\n1.Missed calls\n2.Received calls\n3.Dailled numbers\n4.Erase recent call lists\n5.Show call duration\n6.Show call costs\n7.Call cost settings\n8.Prepaid credit\n";
+        String callRegister = 
+        """
+        Call register
+        1.Missed calls
+        2.Received calls
+        3.Dailed calls
+        4.Erase recent call lists
+        5.Show call duration
+        6.Show call costs
+        7.Call cost settings
+        8.Prepaid credit
+        
+        select option
+        """;
         
         // tones menu
-        String tones = "Tones\n1.Ringing tone\n2.Ringing volume\n3.Incoming call alert\n4.Composer\n5.Keypad tones\n6.Warning and game tones\n7.Vibrating alert\n8.Screen saver\n";
+        String tones = 
+        """
+        Tones
+        1.Ringing tone
+        2.Ringing volume
+        3.Incoming call alert
+        4.Composer
+        5.Keypad tones
+        6.Warning and game tones
+        7.Vibrating alert
+        8.Screen saver
+        
+        select option
+        """;
         
         // settings menu
-        String settings = "Settings\n1.Call settings\n2.Phone settings\n3.Security settings\n4.Restore factory settings\n";
+        String settings =
+        """
+        Settings
+        1.Call settings
+        2.Phone settings
+        3.Security settings
+        4.Restore factory settings
+        
+        select option
+        """;
         
         // call divert menu
-        String callDivert = "Call divert\n1.Call divert\n";
+        String callDivert =
+        """
+        Call divert
+        1.Call divert
+        
+        select option
+        """;
         
         // games menu
-        String games = "Games\n1.Games\n";
+        String games = 
+        """
+        Games
+        1.Games
+        
+        select option
+        """;
         
         // calculator menu
-        String calculator = "Calculator\n1.Calculator\n";
+        String calculator =
+        """
+        Calculator
+        1.Calculator
+        
+        select option
+        """;
         
         // reminders menu
-        String reminders = "Reminders\n1.Reminders\n";
+        String reminders =
+        """
+        Reminders
+        1.Reminders
+        
+        select option
+        """;
         
         // clock menu
-        String clock = "Clock\n1.Clock\n";
+        String clock =
+        """
+        Clock
+        1.Alarm clock
+        2.Clock settings
+        3.Date setting
+        4.Stopwatch
+        5.Countdown timer
+        6.Auto update of time and date
+        """;
         
         // profiles menu
-        String profiles = "Profiles\n1.Profiles\n";
+        String profiles =
+        """
+        Profiles
+        1.Profiles
+        
+        select option
+        """;
         
         // sim services menu
-        String simServices = "Sim services\n1.Clock\n";
+        String simServices =
+        """
+        SIM
+        1.SIM services
+        """;
 
         
-        System.out.println("Please select a menu option");
         int userInput = scanner.nextInt();
         
         switch(userInput){
             case 1->{
-                System.out.println(phoneBook);
+                System.out.println(phoneMenu);
                 userInput = scanner.nextInt();
                 if(userInput==8){
                     System.out.println(phoneSubMenu);
                 }
-                System.out.println("select an option\n0 to return\n");
                 userInput = scanner.nextInt();
                 if(userInput==0){
-                    System.out.println(phoneBook);
+                    System.out.println(phoneMenu);
                 }
-                System.out.println("0 to return to main menu\n");
                 userInput = scanner.nextInt();
                 if(userInput==0){
                     System.out.println(mainMenu);
@@ -74,7 +234,14 @@ public class NokiaPhone {
                 
                 
             }
-            case 2->System.out.println(messages);
+            // messages menu options deep-linking logic
+            case 2->{
+                System.out.println(messages);
+                userInput = scanner.nextInt();
+                if(userInput==7){
+                    System.out.println(mSubMenu);
+                }
+            }
             case 3->System.out.println(chat);
             case 4->System.out.println(callRegister);
             case 5->System.out.println(tones);
