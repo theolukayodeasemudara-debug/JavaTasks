99c5764311f7c0ad2729c8486db2469a2cdf1852
Olukayode <theolukayodeasemudara@gmail.com>
2026-04-15 17:06:20 +0100

    stopped at two

---
+++ FireDrillTwo/TaskFour.java~
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class TaskFour{
+    public static void main (String[] args){
+            Scanner inputCollector = new Scanner(System.in);
+        
+        // collect user input 10 times
+        for(int count =1;count <= 10; count++){   
+            System.out.print("Enter number " + count + ": ");
+            int userNum = inputCollector.nextInt();
+            
+            System.out.print(userNum);
+        }   
+    }
+}
---
+++ FireDrillTwo/TaskOne.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class TaskOne{
+    public static void main (String[] args){
+        Scanner inputCollector = new Scanner(System.in);
+        int total = 0;
+        
+        // collect user input 10 times
+        int count;
+        for(count =1;count <= 10; count++){   
+            System.out.print("Enter number " + count + ": ");
+            int userNum = inputCollector.nextInt();
+            
+            // sum of input collected
+            total +=userNum;
+            System.out.print(total);
+        }   
+    }
+}
