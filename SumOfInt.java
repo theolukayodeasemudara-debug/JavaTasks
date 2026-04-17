83468ac48fa68f3465edde117b831dd2a2163e44
Olukayode <theolukayodeasemudara@gmail.com>
2026-04-17 02:16:34 +0100

    sum of digits refactored to help me understand it better

--- SumOfInt.java
+++ SumOfInt.java
@@ -2,15 +2,23 @@
 import java.util.Scanner;
 public class SumOfInt{
     public static void main(String[] args){
-        Scanner inputCollector = new Scanner(System.in);
-        System.out.println("Enter your digits");
-        int intSquare = inputCollector.nextInt();
-
-        for (;intSquare != 0;) {
-            int digit = intSquare % 10;     // 1. grab last digit
-            int square = digit  digit;     // 2. sum it
-            System.out.print(square + " "); // 3. print the square
-            intSquare = intSquare / 10;     // 4. remove last digit
+        Scanner inputTaker = new Scanner(System.in);
+        int sum = 0;  
+        System.out.println("Enter a value: ");
+        int num = inputTaker.nextInt();
+        
+       // int count = 0;
+        while(num != 0){
+            int digit = num%10;
+            sum += digit;
+            num /= 10;
         }
+        System.out.println("sum of digit is " + num + " " + sum);
+        
     }
 }
+
+            // int digit = num % 10;     // 1. grab last digit
+            // int sum += digit;     // 2. sum it
+            // System.out.print(sum + " "); // 3. print the square
+            // num = num / 10; 
